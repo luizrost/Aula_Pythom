8c96802429126e74a46d788f3997782b375990f3
luizrost <orerost@hotmail.com>
2024-06-06 14:17:02 -0300

    Add files via upload

---
+++ OlaMundo.py
@@ -0,0 +1,8 @@
+nome= "Luiz Rost"
+idade= 39
+cidade= "Mogi das Cruzes"
+Formação= "Estudando Python"
+
+print (f"Ola me chamo{nome} e tenho {idade} anos")
+print (f"Moro em {cidade} e estou atualmente estudando {Formação}")
+ 
